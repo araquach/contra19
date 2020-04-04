0d51ece5eb001dfd0dd8aea6320b88d3ef60b14d
Adam Carter <adam@contra-design.com>
2020-04-04 08:57:01 +0100

    Removed redirect code

--- main.go
+++ main.go
@@ -32,17 +32,17 @@ func init() {
 	}
 }
 
-func redirect(w http.ResponseWriter, req *http.Request) {
-	// remove/add not default ports from req.Host
-	target := "https://" + req.Host + req.URL.Path
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
-	}
-	log.Printf("redirect to: %s", target)
-	http.Redirect(w, req, target,
-		// see comments below and consider the codes 308, 302, or 301
-		http.StatusTemporaryRedirect)
-}
+//func redirect(w http.ResponseWriter, req *http.Request) {
+//	// remove/add not default ports from req.Host
+//	target := "https://" + req.Host + req.URL.Path
+//	if len(req.URL.RawQuery) > 0 {
+//		target += "?" + req.URL.RawQuery
+//	}
+//	log.Printf("redirect to: %s", target)
+//	http.Redirect(w, req, target,
+//		// see comments below and consider the codes 308, 302, or 301
+//		http.StatusTemporaryRedirect)
+//}
 
 func main() {
 	var err error
@@ -60,7 +60,7 @@ func main() {
 	}
 
 	// redirect every http request to https
-	go http.ListenAndServe(":80", http.HandlerFunc(redirect))
+	//go http.ListenAndServe(":80", http.HandlerFunc(redirect))
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home).Methods("GET")
