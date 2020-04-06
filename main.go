c676d480182f8f53e02068d99c39ac914475414a
Adam Carter <adamcarter@jakatasalon.co.uk>
2020-04-06 21:28:02 +0100

    Added env for forceSSL

--- main.go
+++ main.go
@@ -34,10 +34,12 @@ func init() {
 
 func forceSsl(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("x-forwarded-proto") != "https" {
-			sslUrl := "https://" + r.Host + r.RequestURI
-			http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
-			return
+		if os.Getenv("GO_ENV") == "production" {
+			if r.Header.Get("x-forwarded-proto") != "https" {
+				sslUrl := "https://" + r.Host + r.RequestURI
+				http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
+				return
+			}
 		}
 		next.ServeHTTP(w, r)
 	})
