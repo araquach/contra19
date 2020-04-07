8cbfaa0332896dd14a30fddbd594af3c26ac5b04
Adam Carter <adam@contra-design.com>
2020-04-07 13:17:45 +0100

    created handlers file

---
+++ cmd/handlers.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/mailgun/mailgun-go/v3"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func forceSsl(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if os.Getenv("GO_ENV") == "production" {
+			if r.Header.Get("x-forwarded-proto") != "https" {
+				sslUrl := "https://" + r.Host + r.RequestURI
+				http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := tplHome.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func apiSendMessage(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var data ContactMessage
+	err := decoder.Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+
+	mg := mailgun.NewMailgun(os.Getenv("MAILGUN_DOMAIN"), os.Getenv("MAILGUN_KEY"))
+
+	sender := "info@contra-design.com"
+	subject := "New Message for Contra-Design"
+	body := data.Message
+	recipient := "adam@jakatasalon.co.uk"
+
+	// The message object allows you to add attachments and Bcc recipients
+	message := mg.NewMessage(sender, subject, body, recipient)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message	with a 10 second timeout
+	resp, id, err := mg.Send(ctx, message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
+
+	return
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/mailgun/mailgun-go/v3"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 var (
@@ -30,19 +25,6 @@ func init() {
 	if err := godotenv.Load(); err != nil {
 		log.Print("No .env file found")
 	}
-}
-
-func forceSsl(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if os.Getenv("GO_ENV") == "production" {
-			if r.Header.Get("x-forwarded-proto") != "https" {
-				sslUrl := "https://" + r.Host + r.RequestURI
-				http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
-				return
-			}
-		}
-		next.ServeHTTP(w, r)
-	})
 }
 
 func main() {
@@ -77,45 +59,4 @@ func main() {
 	log.Printf("Starting server on %s", port)
 
 	http.ListenAndServe(":" + port, forceSsl(r))
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	if err := tplHome.Execute(w, nil); err != nil {
-		panic(err)
-	}
-}
-
-func apiSendMessage(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-
-	var data ContactMessage
-	err := decoder.Decode(&data)
-	if err != nil {
-		panic(err)
-	}
-
-	mg := mailgun.NewMailgun(os.Getenv("MAILGUN_DOMAIN"), os.Getenv("MAILGUN_KEY"))
-
-	sender := "info@contra-design.com"
-	subject := "New Message for Contra-Design"
-	body := data.Message
-	recipient := "adam@jakatasalon.co.uk"
-
-	// The message object allows you to add attachments and Bcc recipients
-	message := mg.NewMessage(sender, subject, body, recipient)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-
-	// Send the message	with a 10 second timeout
-	resp, id, err := mg.Send(ctx, message)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("ID: %s Resp: %s\n", id, resp)
-
-	return
 }
